00b6f645b70b64a2d615d4a0e09a004c4fa8aa90
KsuKuper <nekrashevich.k@mail.ru>
2023-12-30 17:47:50 +0300

    added scripts

--- demo/script.js
+++ demo/script.js
@@ -2,3 +2,11 @@ let y = 5;
 let x = 10;
 let z = x + y;
 console.log(z);
+console.log(y);
+console.log(x);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Look, how pretty it is, ${fName}`);
+};
+
+sayHelloLinting("Oksana");
